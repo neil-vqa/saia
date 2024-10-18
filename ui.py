b736fc6005dd322ba343a8df3bfa191274d533cd
Neil Van Alino <nvq.alino@gmail.com>
2024-10-18 14:02:20 +0800

    remove writer model

--- ui.py
+++ ui.py
@@ -6,12 +6,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-math_model_url = os.environ.get("MATH_MODEL", "http://localhost:8707")
-writer_model_url = os.environ.get("WRITER_MODEL", "http://localhost:8704")
+math_model_url = os.environ.get("MATH_MODEL", "http://localhost:8708")
 
 stt_model = WhisperModel("medium.en", device="cpu", compute_type="int8")
 math_model = openai.OpenAI(base_url=math_model_url, api_key="sk-no-key-required")
-writer_model = openai.OpenAI(base_url=writer_model_url, api_key="sk-no-key-required")
 
 
 st.set_page_config(page_title="Sansa")
@@ -61,7 +59,7 @@ def entry():
             msg_compute = [
                 {
                     "role": "system",
-                    "content": "You will assist in computing sales transactions. Reason step by step, and put your final answer within \\boxed{}.",
+                    "content": "You will assist in computing sales transactions in pesos as the currency or monetary unit. Reason step by step, and give a preliminary answer. Review your steps, computation accuracy, and the preliminary answer. Then, give your final answer within \\boxed{}.",
                 },
                 {
                     "role": "user",
@@ -70,19 +68,7 @@ def entry():
             ]
             math_res = call_llm(msg_compute, math_model)
 
-            msg_edit = [
-                {
-                    "role": "system",
-                    "content": "You will edit a transaction record to use the appropriate currency.",
-                },
-                {
-                    "role": "user",
-                    "content": f"Text:\n\n{math_res}\n\n\nTask: Identify the currency or unit being used in the text, then replace it to use Philippine Pesos as the currency or unit. Do not explain how, why, or what you edited. Do not paraphrase the text.",
-                },
-            ]
-            writer_res = call_llm(msg_edit, writer_model)
-
-        st.markdown(f"#### Transaction computed:\n\n{writer_res}")
+        st.markdown(f"#### Transaction computed:\n\n{math_res}")
 
 
 if __name__ == "__main__":
