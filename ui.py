be40e746ee8486313cbd96b92b46973b09ebcf71
Neil Van Alino <nvq.alino@gmail.com>
2024-10-19 22:55:32 +0800

    add verification

--- ui.py
+++ ui.py
@@ -8,15 +8,15 @@ from transformers import pipeline
 load_dotenv()
 
 math_model_url = os.environ.get("MATH_MODEL", "http://localhost:8708")
-writer_model_url = os.environ.get("WRITER_MODEL", "http://localhost:8706")
+coder_model_url = os.environ.get("CODER_MODEL", "http://localhost:8706")
 
 stt_model = WhisperModel("medium.en", device="cpu", compute_type="int8")
 math_model = openai.OpenAI(base_url=math_model_url, api_key="sk-no-key-required")
-writer_model = openai.OpenAI(base_url=writer_model_url, api_key="sk-no-key-required")
+coder_model = openai.OpenAI(base_url=coder_model_url, api_key="sk-no-key-required")
 classifier_model = pipeline(
     "zero-shot-classification", model="tasksource/deberta-base-long-nli"
 )
-query_labels = ["sales transaction"]
+query_labels = ["order transaction"]
 
 
 st.set_page_config(page_title="Sansa")
@@ -59,26 +59,26 @@ def call_llm(messages, client, temperature=0.7):
     return res.choices[0].message.content
 
 
-def parse_transaction(transcript):
-    with st.spinner("Writing equation..."):
+def compute_by_symbolic(transcript):
+    with st.spinner("Writing equation and solving..."):
         msg_compute = [
             {
                 "role": "system",
-                "content": "You will write python code that are sympy-compatible algebraic expressions to solve the problems. The code should should be a function, and store the answer in the 'answer' variable",
+                "content": "You will write python code that are sympy-compatible algebraic expressions to solve the problems. The code should be a function, and store the answer in the 'answer' variable",
             },
             {
                 "role": "user",
-                "content": f"Write an algebraic expression to solve this problem:\n\n\n{transcript}\n\n\nOutput should be directly the code block or function. No explanations.",
+                "content": f"Write an algebraic expression to solve this problem:\n\n\n{transcript}\n\n\nStrictly no explanations outside the code block.",
             },
         ]
-        math_res = call_llm(msg_compute, writer_model)
+        math_res = call_llm(msg_compute, coder_model)
 
-    st.markdown(f"#### Transaction equation:\n\n{math_res}")
+    st.markdown(f"#### Symbolic computation:\n\n{math_res}")
     return math_res
 
 
-def compute_transaction(transcript):
-    with st.spinner("Computing..."):
+def compute_by_llm(transcript):
+    with st.spinner("Understanding the problem and computing..."):
         msg_compute = [
             {
                 "role": "system",
@@ -91,7 +91,33 @@ def compute_transaction(transcript):
         ]
         math_res = call_llm(msg_compute, math_model)
 
-    st.markdown(f"#### Transaction computed:\n\n{math_res}")
+    st.markdown(f"#### LLM-based computation:\n\n{math_res}")
+    return math_res
+
+
+def verify_by_symbolic(problem_transcript, prelim_solution):
+    with st.spinner("Verifying..."):
+        msg_compute = [
+            {
+                "role": "system",
+                "content": "You will write python code that are sympy-compatible algebraic expressions to verify solutions to problems. The code should be a function, and store the answer in the 'answer' variable",
+            },
+            {
+                "role": "user",
+                "content": f"The problem:\n\n\n{problem_transcript}\n\n\nPreliminary solution:\n\n\n{prelim_solution}\n\n\nWrite an algebraic expression to solve this problem and verify the preliminary solution. Strictly no explanations outside the code block.",
+            },
+        ]
+        math_res = call_llm(msg_compute, coder_model)
+
+    st.markdown(f"#### Symbolic verification:\n\n{math_res}")
+    return math_res
+
+
+def execute_code(expression):
+    e = expression.replace("```", "")
+    code = e.replace("python", "")
+    exec(code, globals())
+    st.markdown(f"Answer: {answer}")
 
 
 def entry():
@@ -104,17 +130,12 @@ def entry():
 
         output = classifier_model(transcript, query_labels)
         if output["scores"][0] > 0.8:
-            expression = parse_transaction(transcript)
-
-            e = expression.replace("```", "")
-            code = e.replace("python", "")
-            exec(code, globals())
-            st.markdown(f"Answer using python code: {answer}")
-
-            compute_transaction(transcript)
+            prelim = compute_by_llm(transcript)
+            expression = verify_by_symbolic(transcript, prelim)
+            execute_code(expression)
 
         else:
-            st.markdown("Your query is not a sales transaction.")
+            st.markdown("Your query is not an order transaction.")
 
 
 if __name__ == "__main__":
