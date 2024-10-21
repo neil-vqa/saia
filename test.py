3bec74eaa9417a1b879e7d5a6a886eebce77ce5d
Neil Van Alino <nvq.alino@gmail.com>
2024-10-21 19:44:11 +0800

    add tests

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-test_set = {
-    "query_1": "The customer ordered a Spanish Latte at 110 pesos with a 5% discount, an Americano at 100 pesos without a discount, and a Strawberry Matcha at 105 pesos with a 20% discount. How much will the customer pay?",
-    "query_2": "The customer ordered a Spanish latte at 110 pesos with a 5% discount and also a strawberry matcha at 120 pesos without a discount. How much will the customer pay?",
-}
+test_set = [
+    {
+        "query": "The customer ordered a Spanish Latte at 110 pesos with a 5% discount, an Americano at 100 pesos without a discount, and a Strawberry Matcha at 105 pesos with a 20% discount. How much will the customer pay?",
+        "answer": "288.5",
+    },
+    {
+        "query": "The customer ordered a Spanish latte at 110 pesos with a 5% discount and also a strawberry matcha at 120 pesos without a discount. How much will the customer pay?",
+        "answer": "224.5",
+    },
+    {
+        "query": "the customer ordered five Spanish latte at 110 pesos and three strawberry matcha at 95 pesos how much will the customer pay",
+        "answer": "835",
+    },
+]
